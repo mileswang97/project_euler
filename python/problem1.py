69d6059d49a334632661aa147e71e1dd12d51a00
mileswang97 <mileswang97@gmail.com>
2021-03-06 00:56:21 -0500

    p1

---
+++ python/problem1.py
@@ -0,0 +1,15 @@
+import math
+import typing
+
+def solution(n: int):
+    n_list = range(1,n)
+    three_set = set(filter(lambda x: x % 3 == 0, n_list))
+    five_set = set(filter(lambda x: x % 5 == 0, n_list))
+    union = three_set.union(five_set)
+    return sum(union)
+
+print(solution(1000))
+
+print(solution(10))
+
+print(sum(range(1,1000)))
