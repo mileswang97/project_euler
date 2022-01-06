4c34814a1210aac94f3739cac59f55dc42d0c677
mileswang97 <mileswang97@gmail.com>
2022-01-05 21:20:35 -0500

    Update 4.py

--- advent_of_code/4.py
+++ advent_of_code/4.py
@@ -1,6 +1,21 @@
+from itertools import zip_longest
+
+def grouper(iterable, n, fillvalue=None):
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
+
 with open("inputs/4a.txt", "r") as file:
     lines = file.readlines()
     for index in range(len(lines)):
         lines[index] = lines[index].replace("\n", "")
+    lines = [line for line in lines if line != '']
+    calls = lines[0]
+    cards = lines[1::]
 
-print(lines)
+cards = list(grouper(cards, 5))
+
+def array_like(n_tuple):
+
+    return 
+
+print(cards)
